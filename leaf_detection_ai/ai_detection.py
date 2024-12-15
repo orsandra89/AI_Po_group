f9495167d6992fd81b94092c1500ad33acbb7a54
Marek Zulinski <marek.zulinski@student.po.edu.pl>
2024-12-15 22:38:03 +0100

    Add_predicted_image_function

--- leaf_detection_ai/ai_detection.py
+++ leaf_detection_ai/ai_detection.py
@@ -46,3 +46,25 @@ val_generator = datagen.flow_from_directory(
 checkpoint = ModelCheckpoint('best_model.keras', monitor='val_accuracy', save_best_only=True, mode='max')
 reduce_lr = ReduceLROnPlateau(monitor='val_loss', factor=0.2, patience=5, min_lr=1e-6)
 early_stop = EarlyStopping(monitor='val_loss', patience=10, restore_best_weights=True)
+
+def predict_image(img_path):
+    img = image.load_img(img_path, target_size=IMAGE_SIZE)
+    img_array = image.img_to_array(img) / 255.0
+    img_array = np.expand_dims(img_array, axis=0)
+    prediction = model_pretraining_configuration().predict(img_array)
+    class_idx = np.argmax(prediction)
+    class_label = list(train_generator.class_indices.keys())[class_idx]
+    print(f"Predicted Disease: {class_label}")
+# Example usage
+# predict_image('../dataset/train/Apple leaf/Apple-Leaves-Diabetes.jpg')
+
+
+model, history = model_training_configuration()
+
+# Create Visualization instance
+viz = Visualization(model=model, history=history)
+
+# Generate visualizations
+viz.visualise_accuracy_and_loss()
+viz.visualise_dist_images_per_class()
+viz.visualise_learning_curve()
